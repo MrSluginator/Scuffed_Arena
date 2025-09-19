3804aa283fb1edddb54e2ce2bf7a06ea688804c5
MrSluginator <fred@dilger.ca>
2025-09-18 19:37:38 -0600

    Added label spam
    You can now create a label at the characters position by pressing 'space'

--- scuffed_arena/player.gd
+++ scuffed_arena/player.gd
@@ -1,24 +1,32 @@
 extends Area2D
 
 @export var speed = 400
+@onready var label = $Label
 
 
 
 func _ready():
-    print("Hello, world!")
+	print("Hello, world!")
 
 func _process(delta):
-    var velocity = Vector2.ZERO # The player's movement vector.
-    if Input.is_action_pressed("move_right"):
-        velocity.x += 1
-    if Input.is_action_pressed("move_left"):
-        velocity.x -= 1
-    if Input.is_action_pressed("move_down"):
-        velocity.y += 1
-    if Input.is_action_pressed("move_up"):
-        velocity.y -= 1
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_just_pressed("ui_accept"):
+		var copy : Label
+		copy = label.duplicate()
+		copy.show()
+		copy.global_position = position
+		get_parent().add_child(copy)
 
-    if velocity.length() > 0:
-        velocity = velocity.normalized() * speed
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
 
-    position += velocity * delta
+	position += velocity * delta
+	
